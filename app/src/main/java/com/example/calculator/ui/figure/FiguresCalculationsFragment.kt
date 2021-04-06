bbcfa85b142c7f05f4101d08fae83cafa110913e
gethiskg <atai2497@gmail.com>
2021-04-06 13:21:21 +0600

    added animation rotation on transform button

--- app/src/main/java/com/example/calculator/ui/figure/FiguresCalculationsFragment.kt
+++ app/src/main/java/com/example/calculator/ui/figure/FiguresCalculationsFragment.kt
@@ -2,8 +2,12 @@ package com.example.calculator.ui.figure
 
 import android.animation.*
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.calculator.adapter.FiguresAdapter
 import com.example.calculator.base.BaseFragment
@@ -51,10 +55,10 @@ class FiguresCalculationsFragment: BaseFragment<FragmentFiguresCalculationsBindi
         viewModel.layoutLiveData.observe(viewLifecycleOwner) {
             if (it) {
                 val fadeOut = ObjectAnimator.ofFloat(binding.gridMore, "alpha", 0f, 1f)
-                fadeOut.duration = 300
+                fadeOut.duration = 200
 
                 val fadeIn = ObjectAnimator.ofFloat(binding.gridNormal, "alpha", 1f, 0f)
-                fadeIn.duration = 300
+                fadeIn.duration = 200
 
                 fadeIn.addListener(object : Animator.AnimatorListener {
                     override fun onAnimationStart(animation: Animator?) {}
@@ -62,7 +66,9 @@ class FiguresCalculationsFragment: BaseFragment<FragmentFiguresCalculationsBindi
                         binding.gridNormal.setVisibility(!it)
                         binding.gridMore.setVisibility(it)
                         fadeOut.start()
+
                     }
+
                     override fun onAnimationEnd(animation: Animator?) {}
                     override fun onAnimationCancel(animation: Animator?) {}
                     override fun onAnimationRepeat(animation: Animator?) {}
@@ -84,105 +90,121 @@ class FiguresCalculationsFragment: BaseFragment<FragmentFiguresCalculationsBindi
                         binding.gridMore.setVisibility(it)
                         fadeOut.start()
                     }
+
                     override fun onAnimationEnd(animation: Animator?) {}
                     override fun onAnimationCancel(animation: Animator?) {}
                     override fun onAnimationRepeat(animation: Animator?) {}
                 })
 
                 fadeIn.start()
+
             }
+                if (!binding.gridMore.isVisible) {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        binding.figuresTransform.animate().setDuration(350).rotationBy(180f)
+                    }, 500)
+                } else {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        binding.figuresTransformNormal.animate().setDuration(350).rotationBy(180f)
+                    }, 500)
+                }
         }
     }
 
-    private fun showResultAnimation(startSize: Float, endSize: Float, animationDuration: Long, view: TextView) {
-        val animator = ValueAnimator.ofFloat(startSize, endSize)
-        animator.duration = animationDuration
-        animator.addUpdateListener { valueAnimator ->
-            val animatedValue = valueAnimator.animatedValue as Float
-            view.textSize = animatedValue
+        private fun showResultAnimation(
+            startSize: Float,
+            endSize: Float,
+            animationDuration: Long,
+            view: TextView
+        ) {
+            val animator = ValueAnimator.ofFloat(startSize, endSize)
+            animator.duration = animationDuration
+            animator.addUpdateListener { valueAnimator ->
+                val animatedValue = valueAnimator.animatedValue as Float
+                view.textSize = animatedValue
+            }
+            animator.start()
         }
-        animator.start()
+
+        private fun setupUI() {
+            adapter = FiguresAdapter()
+            (binding.recyclerView.layoutManager as? LinearLayoutManager)?.stackFromEnd = false
+            (binding.recyclerView.layoutManager as? LinearLayoutManager)?.reverseLayout = true
+            binding.recyclerView.adapter = adapter
+
+            //clear delete
+            binding.figuresClear.setOnClickListener(this)
+            binding.figuresDelete.setOnClickListener(this)
+
+            binding.figuresClearNormal.setOnClickListener(this)
+            binding.figuresDeleteNormal.setOnClickListener(this)
+
+            // figures
+            binding.figuresZero.setOnClickListener(this)
+            binding.figuresOne.setOnClickListener(this)
+            binding.figuresTwo.setOnClickListener(this)
+            binding.figuresThree.setOnClickListener(this)
+            binding.figuresFour.setOnClickListener(this)
+            binding.figuresFive.setOnClickListener(this)
+            binding.figuresSix.setOnClickListener(this)
+            binding.figuresSeven.setOnClickListener(this)
+            binding.figuresEight.setOnClickListener(this)
+            binding.figuresNine.setOnClickListener(this)
+
+            binding.figuresZeroNormal.setOnClickListener(this)
+            binding.figuresOneNormal.setOnClickListener(this)
+            binding.figuresTwoNormal.setOnClickListener(this)
+            binding.figuresThreeNormal.setOnClickListener(this)
+            binding.figuresFourNormal.setOnClickListener(this)
+            binding.figuresFiveNormal.setOnClickListener(this)
+            binding.figuresSixNormal.setOnClickListener(this)
+            binding.figuresSevenNormal.setOnClickListener(this)
+            binding.figuresEightNormal.setOnClickListener(this)
+            binding.figuresNineNormal.setOnClickListener(this)
+
+            //operations
+            binding.figuresPresantage.setOnClickListener(this)
+            binding.figuresDivide.setOnClickListener(this)
+            binding.figuresMultiple.setOnClickListener(this)
+            binding.figuresMinus.setOnClickListener(this)
+            binding.figuresPlus.setOnClickListener(this)
+            binding.figuresDot.setOnClickListener(this)
+
+            binding.figuresPresantageNormal.setOnClickListener(this)
+            binding.figuresDivideNormal.setOnClickListener(this)
+            binding.figuresMultipleNormal.setOnClickListener(this)
+            binding.figuresMinusNormal.setOnClickListener(this)
+            binding.figuresPlusNormal.setOnClickListener(this)
+            binding.figuresDotNormal.setOnClickListener(this)
+
+            //extra
+            binding.figuresSecondD.setOnClickListener(this)
+
+            binding.figuresDeg.setOnClickListener(this)
+            binding.figuresSin.setOnClickListener(this)
+            binding.figuresCos.setOnClickListener(this)
+            binding.figuresTan.setOnClickListener(this)
+
+            binding.figuresTransform.setOnClickListener(this)
+            binding.figuresTransformNormal.setOnClickListener(this)
+
+            binding.figuresXY.setOnClickListener(this)
+            binding.figuresLg.setOnClickListener(this)
+            binding.figuresIn.setOnClickListener(this)
+            binding.figuresLeftParenthesis.setOnClickListener(this)
+            binding.figuresRightParenthesis.setOnClickListener(this)
+            binding.figuresUnderX.setOnClickListener(this)
+            binding.figuresXExclamation.setOnClickListener(this)
+            binding.figuresOneDivideX.setOnClickListener(this)
+            binding.figuresPConstant.setOnClickListener(this)
+            binding.figuresE.setOnClickListener(this)
+
+            //imageView
+            binding.figuresEqual.setOnClickListener(this)
+            binding.figuresEqualNormal.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            viewModel.onButtonClick(v?.id)
+        }
     }
-
-    private fun setupUI() {
-        adapter = FiguresAdapter()
-        (binding.recyclerView.layoutManager as? LinearLayoutManager)?.stackFromEnd = false
-        (binding.recyclerView.layoutManager as? LinearLayoutManager)?.reverseLayout = true
-        binding.recyclerView.adapter = adapter
-
-        //clear delete
-        binding.figuresClear.setOnClickListener(this)
-        binding.figuresDelete.setOnClickListener(this)
-
-        binding.figuresClearNormal.setOnClickListener(this)
-        binding.figuresDeleteNormal.setOnClickListener(this)
-
-        // figures
-        binding.figuresZero.setOnClickListener(this)
-        binding.figuresOne.setOnClickListener(this)
-        binding.figuresTwo.setOnClickListener(this)
-        binding.figuresThree.setOnClickListener(this)
-        binding.figuresFour.setOnClickListener(this)
-        binding.figuresFive.setOnClickListener(this)
-        binding.figuresSix.setOnClickListener(this)
-        binding.figuresSeven.setOnClickListener(this)
-        binding.figuresEight.setOnClickListener(this)
-        binding.figuresNine.setOnClickListener(this)
-
-        binding.figuresZeroNormal.setOnClickListener(this)
-        binding.figuresOneNormal.setOnClickListener(this)
-        binding.figuresTwoNormal.setOnClickListener(this)
-        binding.figuresThreeNormal.setOnClickListener(this)
-        binding.figuresFourNormal.setOnClickListener(this)
-        binding.figuresFiveNormal.setOnClickListener(this)
-        binding.figuresSixNormal.setOnClickListener(this)
-        binding.figuresSevenNormal.setOnClickListener(this)
-        binding.figuresEightNormal.setOnClickListener(this)
-        binding.figuresNineNormal.setOnClickListener(this)
-
-        //operations
-        binding.figuresPresantage.setOnClickListener(this)
-        binding.figuresDivide.setOnClickListener(this)
-        binding.figuresMultiple.setOnClickListener(this)
-        binding.figuresMinus.setOnClickListener(this)
-        binding.figuresPlus.setOnClickListener(this)
-        binding.figuresDot.setOnClickListener(this)
-
-        binding.figuresPresantageNormal.setOnClickListener(this)
-        binding.figuresDivideNormal.setOnClickListener(this)
-        binding.figuresMultipleNormal.setOnClickListener(this)
-        binding.figuresMinusNormal.setOnClickListener(this)
-        binding.figuresPlusNormal.setOnClickListener(this)
-        binding.figuresDotNormal.setOnClickListener(this)
-
-        //extra
-        binding.figuresSecondD.setOnClickListener(this)
-
-        binding.figuresDeg.setOnClickListener(this)
-        binding.figuresSin.setOnClickListener(this)
-        binding.figuresCos.setOnClickListener(this)
-        binding.figuresTan.setOnClickListener(this)
-
-        binding.figuresTransform.setOnClickListener(this)
-        binding.figuresTransformNormal.setOnClickListener(this)
-
-        binding.figuresXY.setOnClickListener(this)
-        binding.figuresLg.setOnClickListener(this)
-        binding.figuresIn.setOnClickListener(this)
-        binding.figuresLeftParenthesis.setOnClickListener(this)
-        binding.figuresRightParenthesis.setOnClickListener(this)
-        binding.figuresUnderX.setOnClickListener(this)
-        binding.figuresXExclamation.setOnClickListener(this)
-        binding.figuresOneDivideX.setOnClickListener(this)
-        binding.figuresPConstant.setOnClickListener(this)
-        binding.figuresE.setOnClickListener(this)
-
-        //imageView
-        binding.figuresEqual.setOnClickListener(this)
-        binding.figuresEqualNormal.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        viewModel.onButtonClick(v?.id)
-    }
-}
